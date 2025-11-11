c4def4fb30f0bbf1980df7fcd4040a66b8bbf1d1
Manahil4 <manahilejaz16@gmail.com>
2025-11-11 16:38:45 +0500

    week2day2

--- week2/day1/practStrings.cpp
+++ week2/day1/practStrings.cpp
@@ -4,5 +4,14 @@ using namespace std;
 
 int main() {
   string car="TOYOTA";
-  for(int c:car){
+  const char carChar[]="ALTO";//here size of arr will be determine by compler
+ char charArr[4] = {'w', 's', 'd', 'm'}; 
+  const char carChar[5]="ALTO";//not 4 elements, 5 including \n
+  foreach( int c:car){
   cout <<car[c];}}
+cout <<"Printing char arr / string literal"; 
+  forreach(char c:carChar){
+  cout <<c;
+  }
+  
+  }
